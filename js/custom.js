b96b30999ee48039a6866807aea197ee3c717ec8
Rahul12749 <74234883+Rahul12749@users.noreply.github.com>
2023-07-24 10:24:00 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+$(function() {
+
+	'use strict';
+var slider2 = new Swiper ('.slider2', {
+    effect: 'fade',
+    autoplay: true
+ },
+);
+var slider3 = new Swiper ('.slider3', {
+    effect: 'fade',
+    autoplay: {
+   delay: 1500,
+ },
+});
+}
+);
